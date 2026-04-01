279740225d48bb901b809e4e95f560cec3be4a20
Dawid Tyburek <dawidtyburek@user.com>
2026-04-02 00:32:18 +0200

    chore: revert to simple Package.swift without wrapper target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -12,25 +12,12 @@ let package = Package(
             targets: ["UserComSDK"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0"),
-        .package(url: "https://github.com/kaishin/Gifu.git", exact: "3.3.1")
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
-            name: "UserComSDKBinary",
-            url: "https://github.com/UserEngage/iOS-SDK/releases/download/0.7.11/UserComSDK.xcframework.zip",
-            checksum: "aac3948fcb84632bbecdf4d82781bd6457193cc0aa7d20f92ba674517b0dae8b"
-        ),
-        .target(
             name: "UserComSDK",
-            dependencies: [
-                .target(name: "UserComSDKBinary"),
-                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
-                .product(name: "Gifu", package: "Gifu")
-            ],
-            path: "Sources/UserComSDKTargets"
+            url: "https://github.com/UserEngage/iOS-SDK/releases/download/0.7.12/UserComSDK.xcframework.zip",
+            checksum: "aac3948fcb84632bbecdf4d82781bd6457193cc0aa7d20f92ba674517b0dae8b"
         )
     ]
 )
