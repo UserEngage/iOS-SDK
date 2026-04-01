5e86b1c4a2182487d38e6c2b8f26bbd444c921eb
Dawid Tyburek <dawidtyburek@user.com>
2026-04-01 10:12:09 +0200

    feat: add Firebase and Gifu as SPM dependencies

--- Package.swift
+++ Package.swift
@@ -12,12 +12,25 @@ let package = Package(
             targets: ["UserComSDK"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0"),
+        .package(url: "https://github.com/kaishin/Gifu.git", exact: "3.3.1")
+    ],
     targets: [
         .binaryTarget(
-            name: "UserComSDK",
+            name: "UserComSDKBinary",
             url: "https://github.com/UserEngage/iOS-SDK/releases/download/0.7.7/UserComSDK.xcframework.zip",
             checksum: "aac3948fcb84632bbecdf4d82781bd6457193cc0aa7d20f92ba674517b0dae8b"
+        ),
+        .target(
+            name: "UserComSDK",
+            dependencies: [
+                .target(name: "UserComSDKBinary"),
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                .product(name: "Gifu", package: "Gifu")
+            ],
+            path: "Sources/UserComSDKTargets"
         )
     ]
 )
